2feede37c07710c717ffc06b9d72ffae19ce9991
Harsh Singh Girase <114455482+harshalsinghgirase@users.noreply.github.com>
2023-05-31 13:06:52 +0530

    Create Program1.java

---
+++ Control Statements/WhileLoop/SimpleWhileLoop1/Program1.java
@@ -0,0 +1,13 @@
+//Program 1: Write a program to print a table of 2
+//        Output: 2 4 6 8 10 12 14 16 18 20
+
+class Program1{
+    public static void main(String[] args) {
+        int x = 2;
+        int n=1;
+        while(n<11){
+            System.out.print(n*x+"  ");
+            n++;
+        }
+    }
+}
